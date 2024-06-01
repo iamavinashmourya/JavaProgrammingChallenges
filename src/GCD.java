19ff8c57d4cb5bd5038e22b5e4920431cab96b6f
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-01 22:29:42 +0530

    Challenge19: Create a program to find the greatest common divisor (GCD) of two numbers

---
+++ src/GCD.java
@@ -0,0 +1,35 @@
+/* Create a program to find the greatest common divisor (GCD) of two numbers.*/
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int first = input.nextInt();
+        System.out.print("Enter a number: ");
+        int second = input.nextInt();
+        int GCDVL = gcdFinder(first,second);
+        System.out.println("GCD is: " +GCDVL);
+    }
+    public static int gcdFinder(int num1, int num2){
+        int gcd=1;
+        int i=2;
+        int least = least(num1,num2);
+        while (i<=least){
+            if (num1 % i == 0 && num2 % i == 0){
+                gcd =i;
+            }
+            i++;
+        }
+        return gcd;
+    }
+    public static int least(int num1, int num2){
+        if (num1<num2){
+            return num1;
+        }
+        else {
+            return num2;
+        }
+    }
+}
