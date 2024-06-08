68a9071768acad8ce63595d4f5486c49e016f2e1
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-08 15:05:29 +0530

    Challenge42: Write a Java method to determine if two given strings are anagrams (contain the same characters in any order).

---
+++ src/AnagramChecker.java
@@ -0,0 +1,20 @@
+/* Write a Java method to determine if two given strings are anagrams (contain the same
+characters in any order). */
+
+import java.util.Arrays;
+
+public class AnagramChecker {
+    public static boolean areAnagrams(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+        System.out.println(areAnagrams(str1, str2));  // Output: true
+    }
+}
