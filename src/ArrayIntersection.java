a721a9c0447657fb97e1d6da07142648d4019694
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-08 15:23:19 +0530

    Challenge48: Write a Java program to find the intersection of two arrays.

---
+++ src/ArrayIntersection.java
@@ -0,0 +1,30 @@
+/* Write a Java program to find the intersection of two arrays. */
+
+import java.util.*;
+
+public class ArrayIntersection {
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.getOrDefault(num, 0) > 0) {
+                result.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        int[] intersection = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            intersection[i] = result.get(i);
+        }
+        return intersection;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 1};
+        int[] nums2 = {2, 2};
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));  // Output: [2, 2]
+    }
+}
