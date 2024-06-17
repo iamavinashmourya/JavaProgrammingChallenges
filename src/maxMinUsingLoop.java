364a25515ad5bd1161d49a733347bb4e0d9e0012
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-17 08:38:37 +0530

    Challenge61: Write program find min and max number using for loop and input from user

---
+++ src/maxMinUsingLoop.java
@@ -0,0 +1,39 @@
+/*
+Write program find min and max number using for loop and input from user
+ */
+
+
+import java.util.Scanner;
+
+public class maxMinUsingLoop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Minimum number: " + min);
+        System.out.println("Maximum number: " + max);
+
+        scanner.close();
+    }
+}
